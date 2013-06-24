3cffd8404195800ddf4542f2f4792ab618721406
Tollef Fog Heen <tfheen@err.no>
2013-06-24 09:55:43 +0200

    Import older packaging

---
+++ debian/RT.pm
@@ -0,0 +1,8 @@
+package RT;
+use vars qw($LocalPath $BasePath $EtcPath $BinPath $VarPath);
+$LocalPath = '/usr/share/request-tracker4';
+$BasePath = '/usr/share/request-tracker4';
+$EtcPath = '/etc/request-tracker4';
+$BinPath = '/usr/bin';
+$VarPath = '/var/cache/request-tracker4';
+
