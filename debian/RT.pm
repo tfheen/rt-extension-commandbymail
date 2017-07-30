2bcb3a25a0ec653bd1a65a482f94cb118641b18f
Tollef Fog Heen <tfheen@err.no>
2017-07-30 10:39:01 +0200

    Set locallibpath in local RT.pm

--- debian/RT.pm
+++ debian/RT.pm
@@ -1,8 +1,8 @@
 package RT;
-use vars qw($LocalPath $BasePath $EtcPath $BinPath $VarPath);
+use vars qw($LocalPath $LocalLibPath $BasePath $EtcPath $BinPath $VarPath);
 $LocalPath = '/usr/share/request-tracker4';
 $BasePath = '/usr/share/request-tracker4';
 $EtcPath = '/etc/request-tracker4';
 $BinPath = '/usr/bin';
 $VarPath = '/var/cache/request-tracker4';
-
+$LocalLibPath = '/usr/local/share/request-tracker4/lib';
